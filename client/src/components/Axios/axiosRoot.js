18a7334fd2cc20d3e9bac69351e1f5594b1ec9cd
Ankush Sinha <97420654+ankushSinha1@users.noreply.github.com>
2023-09-18 21:18:28 +0530

    Minor changes

--- client/src/components/Axios/axiosRoot.js
+++ client/src/components/Axios/axiosRoot.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export const rootRoute = axios.create({
-    // baseURL: 'https://zumeit-server.onrender.com',
-    baseURL: 'http://localhost:4000',
+    baseURL: 'https://zumeit-server.onrender.com',
+    // baseURL: 'http://localhost:4000',
 })
